99aa96178f77fc181a7ff46afef77f979b53a0f4
albihasani94 <albihasani94@gmail.com>
2020-08-16 19:17:04 +0200

    Rewrite component

--- acme-product-management/src/app/app.component.ts
+++ acme-product-management/src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <div>
+    <h1>{{pageTitle}}</h1>
+    <div>My First Component</div>
+  </div>
+  `
 })
 export class AppComponent {
-  title = 'Acme Product Management';
+  pageTitle: string = 'Acme Product Management';
 }
