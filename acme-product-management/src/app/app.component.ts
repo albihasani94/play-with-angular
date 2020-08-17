b1309a92692d0c70c7f1273dae536d0514aafa71
albihasani94 <albihasani94@gmail.com>
2020-08-17 13:45:01 +0200

    Add parameter to route and add guard

--- acme-product-management/src/app/app.component.ts
+++ acme-product-management/src/app/app.component.ts
@@ -3,10 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'pm-root',
   template: `
-  <div>
-    <h1>{{pageTitle}}</h1>
-    <pm-products></pm-products>
-  </div>
+    <nav class='navbar navvbar-expand navbar-light bg-light'>
+        <a class='navbar-brand'>{{pageTitle}}</a>
+        <ul class='nav nav-pills'>
+          <li><a class='nav-link' [routerLink]="['/welcome']">Home</a></li>
+          <li><a class='nav-link' [routerLink]="['/products']">Product List</a></li>
+        </ul>
+        <router-outlet></router-outlet>
+    </nav>
   `
 })
 export class AppComponent {
